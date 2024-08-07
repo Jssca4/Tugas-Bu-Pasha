82a0e8d8e3322f49da390ecad6f1b0828327bb2b
Jssca4 <jessica_ristiani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:22:36 +0700

    Add files via upload

---
+++ TestGuru.java
@@ -0,0 +1,71 @@
+//Driver Class
+public class TestGuru {
+    public static void main(String[] args) {
+        //membuat obyek
+        Guru Aulia = new Guru();
+        Guru Pasha = new Guru();
+        Guru Nasikin = new Guru();
+        Guru Rahmad = new Guru();
+        Guru Diaur = new Guru();
+
+        Aulia.id = 1;
+        Aulia.nama = "Aulia";
+        Aulia.mapel = "Produktif";
+        Aulia.alamat = "Malang";
+        Pasha.id = 2;
+        Pasha.nama = "Pasha";
+        Pasha.mapel = "Produktif";
+        Pasha.alamat = "Malang";
+        Nasikin.id = 3;
+        Nasikin.nama = "Nasikin";
+        Nasikin.mapel = "Bk";
+        Nasikin.alamat = "Malang";
+        Rahmad.id = 4;
+        Rahmad.nama = "Rahmad";
+        Rahmad.mapel = "Ipas";
+        Rahmad.alamat = "Malang";
+        Diaur.id = 5;
+        Diaur.nama = "Diaur";
+        Diaur.mapel = "Pjok";
+        Diaur.alamat = "Malang";
+
+        System.out.println(" Ini Data Aulia");
+        System.out.println(Aulia.id);
+        System.out.println(Aulia.nama);
+        System.out.println(Aulia.mapel);
+        System.out.println(Aulia.alamat);
+        System.out.println(" Ini Data Pasha");
+        System.out.println(Pasha.id);
+        System.out.println(Pasha.nama);
+        System.out.println(Pasha.mapel);
+        System.out.println(Pasha.alamat);
+        System.out.println(" Ini Data Nasikin");
+        System.out.println(Nasikin.id);
+        System.out.println(Nasikin.nama);
+        System.out.println(Nasikin.mapel);
+        System.out.println(Nasikin.alamat);
+        System.out.println(" Ini Data Rahmad");
+        System.out.println(Rahmad.id);
+        System.out.println(Rahmad.nama);
+        System.out.println(Rahmad.mapel);
+        System.out.println(Rahmad.alamat);
+        System.out.println(" Ini Data Diaur");
+        System.out.println(Diaur.id);
+        System.out.println(Diaur.nama);
+        System.out.println(Diaur.mapel);
+        System.out.println(Diaur.alamat);
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    }
+}
